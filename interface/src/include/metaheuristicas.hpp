ff91d456bbd243fd125bba583bb5761b6d29ee0c
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2026-03-16 00:40:55 -0300

    ADD: Classe abstrata de metaheuristicas e CMake

---
+++ interface/src/include/metaheuristicas.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+// configuração da biblioteca para gerar json
+#include "libs/json.hpp"
+using json = nlohmann::json;
+
+/**
+ * @brief Classe abstrata para criação de algoritmos de metaheuristicas
+ */
+class Metaheuristicas {
+    public:
+        virtual ~Metaheuristicas() = default;
+
+        virtual void solve() = 0;
+
+        virtual void setParametros(const json &params) = 0;
+
+        virtual json getResultados() const = 0;
+
+        virtual std::string getNome() const = 0;
+};
